bdef81717fd6557e3577475a8a64d1148ffba932
Raydelto Hernandez <raydelto@.gmail.com>
2019-04-22 12:26:10 -0300

    First working version (WIP)

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,17 @@
 #include <iostream>
 using namespace std;
 
-bool ReadBMP(string imagepath, unsigned char *header, unsigned char *rgbData, unsigned int &imageSize)
+bool WriteBMP(string imagepath, unsigned char *header, unsigned char *rgbData, unsigned int &imageSize, unsigned int &headerSize);
+bool ReadBMP(string imagepath, unsigned char *header, unsigned char *rgbData, unsigned int &imageSize, unsigned int &headerSize);
+
+bool ReadBMP(string imagepath, unsigned char *header, unsigned char *rgbData, unsigned int &imageSize, unsigned int &headerSize)
 {
     //BMP Header
     header = new unsigned char[54]; // 54-bytes Header
     unsigned int dataPos;     // Position where data begins
     unsigned int width;
     unsigned int height;
+    unsigned short bitsPerPixel;
 
     // Open the file
     FILE * file = fopen(imagepath.c_str(),"rb");
@@ -26,17 +30,19 @@ bool ReadBMP(string imagepath, unsigned char *header, unsigned char *rgbData, un
     //If it cannot read at least 54 bytes then this is not a valid BMP File
     //It the first two bytes are not "BM" respectively this is not a valid BMP file
 
-    if ( fread(header, 1, 54, file) != 54 || header[0] != 'B' || header[1] != 'M' ) 
+    if ( fread(header, 1, 138, file) != 138 || header[0] != 'B' || header[1] != 'M' ) 
     { 
         cerr  << "This is not a valid BMP file\n" << endl;
         return false;
     } 
 
     //This header locations are specify on the bitmap format (http://www.fastgraph.com/help/bmp_header_format.html)
-    dataPos    = *(int*)&(header[0x0A]);
-    imageSize  = *(int*)&(header[0x22]);
-    width      = *(int*)&(header[0x12]);
-    height     = *(int*)&(header[0x16]);
+    dataPos    = *(int*)&(header[10]);
+    imageSize  = *(int*)&(header[34]);
+    width      = *(int*)&(header[18]);
+    height     = *(int*)&(header[22]);
+    bitsPerPixel     = *(short*)&(header[28]);
+    
 
     // Setting default values of imageSize and datapos if these were not found on the file
     if (imageSize==0)
@@ -58,32 +64,33 @@ bool ReadBMP(string imagepath, unsigned char *header, unsigned char *rgbData, un
     //Everything is in memory now, the file can be closed
     fclose(file);
 
+    headerSize = dataPos;
+
+    WriteBMP("img/test2.bmp", header, rgbData, imageSize, headerSize);
+
+
     return true;
 }
 
-bool WriteBMP(string imagepath, unsigned char *header, unsigned char *rgbData, unsigned int &imageSize)
+bool WriteBMP(string imagepath, unsigned char *header, unsigned char *rgbData, unsigned int &imageSize, unsigned int &headerSize)
 {
     FILE * file;
     file = fopen(imagepath.c_str(), "wb");
-    fwrite(header , 1, 54, file);
-    fclose (file);
-
-    file = fopen(imagepath.c_str(), "ab");
+    fwrite(header , 1, headerSize, file);
     fwrite(rgbData ,1, imageSize, file);
     fclose(file);    
 }
 
-
 int main( void )
 {
+    
     unsigned char *rgbData;
     unsigned char *header;
     unsigned int imageSize;
+    unsigned int headerSize;
     printf("Read\n");
-    ReadBMP("img/test.bmp", header, rgbData, imageSize);
+    ReadBMP("img/test.bmp", header, rgbData, imageSize, headerSize);
     printf("imageSize = %d \n", imageSize);
-    printf("Write\n");
-    WriteBMP("img/test2.bmp", header, rgbData, imageSize);
     printf("END\n");
     return 0;
 }
