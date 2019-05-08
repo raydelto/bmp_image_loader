aef4fd28202a6f8aab364eb64277e0fa2d1f6bbb
Raydelto Hernandez <raydelto@gmail.com>
2019-05-08 09:46:11 -0300

    Removing extra semicolons on main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -127,14 +127,14 @@ int main( void )
     unsigned char *header;
     unsigned int imageSize;
     unsigned int headerSize;
-    cout << "Reading the BMP file ... " << endl;;
+    cout << "Reading the BMP file ... " << endl;
     ReadBMP("img/test.bmp", header, rgbData, headerSize, imageSize);
 
-    cout << "Writing a new BMP file based on data read from a BPM in the previous step ..." << endl;;
+    cout << "Writing a new BMP file based on data read from a BPM in the previous step ..." << endl;
     WriteBMP("img/test2.bmp", header, rgbData, headerSize, imageSize, false, false);
-    cout << "Freeing resources..." << endl;;
+    cout << "Freeing resources..." << endl;
     delete rgbData;
     delete header;
-    cout << "This application has ended its execution." << endl;;
+    cout << "This application has ended its execution." << endl;
     return 0;
 }
